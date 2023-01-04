04da157b52686801847915958a1b26aeca25066f
edevars <e.devars.dev@gmail.com>
2023-01-03 18:47:28 -0600

    :sparkles: Adding express server

---
+++ src/server/config/index.ts
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const config = {
+  PORT: process.env.PORT
+};
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,12 @@
-console.log("Hola mundo desde el server")
+import express, { Express, Request, Response } from 'express'
+import { config } from './config';
+
+const app: Express = express();
+
+app.get('*', (req: Request, res: Response) => {
+  res.send(`<h1>Hola mundo con ruta: ${req.url}</h1>`)
+})
+
+app.listen(config.PORT, () => {
+  console.log(`Listening in http://localhost:${config.PORT}`)
+})
